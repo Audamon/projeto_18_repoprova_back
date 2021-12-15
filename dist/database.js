ded9f9b28ef6362017d197f710c8f73c78db4844
Audamon <joaotamagno@gmail.com>
2021-12-15 18:14:46 -0300

    dist/

--- dist/database.js
+++ dist/database.js
@@ -37,9 +37,9 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 exports.__esModule = true;
 var typeorm_1 = require("typeorm");
-if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL.indexOf("sslmode=require") === -1) {
-    process.env.DATABASE_URL += "?sslmode=require";
-}
+// if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL.indexOf("sslmode=require") === -1) {
+//   process.env.DATABASE_URL += "?sslmode=require";
+// }
 function connect() {
     return __awaiter(this, void 0, void 0, function () {
         var connectionManager, connection;

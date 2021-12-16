5724cb2dcd0911edafbc11310939a30afedb16a2
Audamon <joaotamagno@gmail.com>
2021-12-16 11:16:24 -0300

    dist/

--- dist/Repositories/createUser.js
+++ dist/Repositories/createUser.js
@@ -35,13 +35,10 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 exports.__esModule = true;
 exports.createUser = void 0;
 var typeorm_1 = require("typeorm");
-var userEntity_1 = __importDefault(require("../Entities/userEntity"));
+// import UserEntity from '../Entities/userEntity';
 // import { Users } from '../Interfaces/userInterface';
 function createUser(email, encryptedPassword, name) {
     return __awaiter(this, void 0, void 0, function () {
@@ -50,11 +47,9 @@ function createUser(email, encryptedPassword, name) {
             switch (_a.label) {
                 case 0:
                     console.log(6);
-                    return [4 /*yield*/, (0, typeorm_1.getRepository)(userEntity_1["default"]).query('INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETUIRNING*;', [name, encryptedPassword, email])];
+                    return [4 /*yield*/, (0, typeorm_1.getManager)().query('INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETUIRNING*;', [name, encryptedPassword, email])];
                 case 1:
                     user = _a.sent();
-                    console.log(email, encryptedPassword, name);
-                    console.log(process.env.DATABASE_URL);
                     return [2 /*return*/, user];
             }
         });
--- src/Repositories/createUser.ts
+++ src/Repositories/createUser.ts
@@ -1,11 +1,9 @@
-import { getRepository } from 'typeorm';
-import UserEntity from '../Entities/userEntity';
+import { getManager } from 'typeorm';
+// import UserEntity from '../Entities/userEntity';
 // import { Users } from '../Interfaces/userInterface';
 
 export async function createUser(email: string, encryptedPassword: string, name: string) {
   console.log(6);
-  const user = await getRepository(UserEntity).query('INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETUIRNING*;', [name, encryptedPassword, email]);
-  console.log(email, encryptedPassword, name);
-  console.log(process.env.DATABASE_URL);
+  const user = await getManager().query('INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETUIRNING*;', [name, encryptedPassword, email]);
   return user;
 }

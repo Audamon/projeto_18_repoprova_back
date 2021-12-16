5909f16c4febab6e943b525e09af6aae78cfbc5a
Audamon <joaotamagno@gmail.com>
2021-12-16 12:17:19 -0300

    dist/

--- src/Services/createUser.ts
+++ src/Services/createUser.ts
@@ -1,4 +1,4 @@
-import { Users } from '../Interfaces/userInterface';
+// import { Users } from '../Interfaces/userInterface';
 import * as createUserRepository from '../Repositories/createUser';
 import * as encryptPasswordService from './encryptPassword';
 
@@ -6,7 +6,7 @@ export async function createUser(
   name: string,
   email: string,
   password: string,
-): Promise<Users> {
+) {
   console.log(3);
   const encryptedPassword = await encryptPasswordService.encryptPassword(password);
   console.log(4);
